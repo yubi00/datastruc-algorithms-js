e9a6b3442b5aed7318e3f0bd879992956a8491b9
yubi00 <ubrajkhadka@gmail.com>
2020-12-21 00:01:15 +1100

    tree traversal

--- binary-tree/bst.js
+++ binary-tree/bst.js
@@ -54,6 +54,57 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  Bfs() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DfsPreOrder() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DfsPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DfsInOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 let bst = new BinarySearchTree();
@@ -61,11 +112,15 @@ let bst = new BinarySearchTree();
 bst.insert(20);
 bst.insert(17);
 bst.insert(15);
+bst.insert(18);
 bst.insert(25);
+bst.insert(50);
+bst.insert(22);
 
-const res = bst.find(137);
-console.log(res);
+const dfs = bst.DfsInOrder();
+console.log(dfs);
 
 //BIG O
+
 // INSERTION = O(logN)
 // SEARCHING - O(logN)
