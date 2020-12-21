e1eb0f5e0a9bce2653069a8f7b5d6a9ead6cf491
yubi00 <ubrajkhadka@gmail.com>
2020-12-21 18:52:37 +1100

    graph traversal

--- data-structures/graphs/graph.js
+++ data-structures/graphs/graph.js
@@ -24,17 +24,82 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          return dfs(neighbour);
+        }
+      });
+    })(start);
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const visited = {};
+    const result = [];
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    visited[start] = true;
+
+    while (queue.length) {
+      const currentVertex = queue.shift();
+      result.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let graph = new Graph();
-graph.addVertex("Kathmandu");
-graph.addVertex("Melbourne");
-graph.addVertex("Sydney");
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-graph.addEdge("Kathmandu", "Melbourne");
-graph.addEdge("Kathmandu", "Sydney");
-graph.addEdge("Melbourne", "Sydney");
-
-graph.removeVertex("Kathmandu");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
 
 console.log(graph.adjacencyList);
+console.log("recursive: ", graph.depthFirstRecursive("A"));
+console.log("iterative: ", graph.depthFirstIterative("A"));
+console.log("breadth first: ", graph.breadthFirst("A"));
